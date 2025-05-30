d6475cd7e3fc8dbab8d9c35cedd6df772399d24f
BorjaMerMck <borjacursos146@gmail.com>
2025-05-30 15:00:04 +0200

    done

--- src/main/java/com/gammatech/coffee/configuration/SwaggerConfig.java
+++ src/main/java/com/gammatech/coffee/configuration/SwaggerConfig.java
@@ -1,5 +1,11 @@
 package com.gammatech.coffee.configuration;
 
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+
 public class SwaggerConfig {
 
     @Bean
@@ -10,7 +16,7 @@ public class SwaggerConfig {
                         .description("Sistema de gestión para cafetería que permite administrar cafés, clientes y pedidos")
                         .version("1.0")
                         .contact(new Contact()
-                                .name("Sandra Amorós Reyes")
-                                .email("nasdrahlovejinx@hotmail.com")));
+                                .name("Borja Merchan Mckenna")
+                                .email("borjamerchan146@gmail.com")));
     }
 }
